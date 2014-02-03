9ee3b2e32b092a22f2fcc48c6b7b9eaf54afdfb8
Orest Hrycyna <orest.hrycyna@surfdome.com>
2014-02-03 12:34:35 +0000

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -9,6 +11,25 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
-  
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end  
+
 end

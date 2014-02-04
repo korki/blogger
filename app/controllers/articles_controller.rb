99ca4163699bfdab2d00457ce9172a30c272ffa8
Jose Pettoruti <jose.pettoruti@surfdome.com>
2014-02-04 15:46:03 +0000

    sidebar started

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
+    @article.save!
     redirect_to article_path(@article)
   end
 
